1709e25b524ec1e9ff8754f6364c573453570dd5
Serge Basangovs <sergejs.basangovs@gmail.com>
2021-06-12 12:09:49 +0100

    nested data schema and reslolvers implemented cross-referencing sessions to speakers

--- server/resolvers.ts
+++ server/resolvers.ts
@@ -1,36 +1,15 @@
 import { IResolvers } from "apollo-server";
 import _ from "lodash";
+import Query from "./resolvers/query";
+import Session from "./resolvers/session";
+import Speaker from "./resolvers/speaker";
 
-const resolvers: IResolvers = {
-  Query: {
-    sessions: (parent, args, { dataSources }, info) => {
-      return dataSources.SessionAPI.getSessions(args);
-    },
-    sessionById: (parent, { id }, { dataSources }, info) => {
-      return dataSources.SessionAPI.getSessionById(id);
-    },
-    speakers: (parent, args, { dataSources }, info) => {
-      return dataSources.SpeakerAPI.getSpeakers();
-    },
-    speakerById: (parent, { id }, { dataSources }, info) => {
-      return dataSources.SpeakerAPI.getSpeakerById(id);
-    },
-  },
-  Session: {
-    speakers: async (session, args, { dataSources }, info) => {
-      const speakers = await dataSources.SpeakerAPI.getSpeakers();
-      const returns = speakers.filter((objSpeaker: Record<string, any>) => {
-        return _.filter(objSpeaker.sessions, { id: session.id }).length > 0;
-      });
-      return returns;
-    },
-  },
-  Speaker: {
-    sessions: async (speaker, args, { dataSources }, info) => {
-      const speakerId = speaker.id;
-      console.log("speakerId :>> ", speakerId);
-    },
-  },
+import { ContextType } from "./types";
+
+const resolvers: IResolvers<any, ContextType> = {
+  Query,
+  Session,
+  Speaker,
 };
 
 export default resolvers;
