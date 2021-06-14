1be48679162ce771060763d2c4beadfc24a124d8
Serge Basangovs <sergejs.basangovs@gmail.com>
2021-06-14 13:48:17 +0100

    removed redundnt code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,36 +3,33 @@ import { Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Media } from "./pages/media/Media";
-import { OurStory } from "./pages/our-story/OurStory";
+import { OurStory } from "./pages/OurStory/OurStory";
 import { Robotics } from "./pages/robotics/Robotics";
 import { Conference } from "./pages/conference/Conference";
 import { Home } from "./pages/home/Home";
-import "./App.css";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <div id="wrapper">
-        <Header />
-        <Switch>
-          <Route path="/media">
-            <Media />
-          </Route>
-          <Route path="/our-story">
-            <OurStory />
-          </Route>
-          <Route path="/robotics">
-            <Robotics />
-          </Route>
-          <Route path="/conference">
-            <Conference />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-        <Footer />
-      </div>
+    <div id="wrapper">
+      <Header />
+      <Switch>
+        <Route path="/media">
+          <Media />
+        </Route>
+        <Route path="/our-story">
+          <OurStory />
+        </Route>
+        <Route path="/robotics">
+          <Robotics />
+        </Route>
+        <Route path="/conference">
+          <Conference />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+      <Footer />
     </div>
   );
 };
