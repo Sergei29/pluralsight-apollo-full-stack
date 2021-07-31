63ef44ed352ed88e2b29b4f1e1e0aef8ead73e4b
Serge Basangovs <sergejs.basangovs@gmail.com>
2021-07-31 15:49:20 +0300

    subscribe to more in sessions list

--- client/src/components/Sessions/components/SessionList/SessionList.tsx
+++ client/src/components/Sessions/components/SessionList/SessionList.tsx
@@ -1,6 +1,7 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { useQuery } from "@apollo/client";
 import { SESSIONS } from "../../../../graphql/queries";
+import { FAVORITES_SUBSCRIBE } from "../../../../graphql/subscriptions";
 import { SessionType, LevelsStateType, Days, UserType } from "../../../types";
 import SessionItem from "../../../SessionItem";
 
@@ -18,12 +19,48 @@ type Props = {
 };
 
 const SessionList: React.FC<Props> = ({ day, objLevels, isDescription }) => {
-  const { loading, error, data } = useQuery<ResponseDataType>(SESSIONS, {
+  const { loading, error, data, subscribeToMore } = useQuery<
+    Record<string, any>
+  >(SESSIONS, {
     variables: { day, isDescription },
   });
   const { bIntro, bIntermediate, bAdvanced } = objLevels;
   const arrFavorites =
-    data?.user.favorites?.map((objSession) => objSession.id) || [];
+    data?.user.favorites?.map((objSession: SessionType) => objSession.id) || [];
+
+  /**
+   * @description effect to run subscription
+   * @returns {undefined}
+   */
+  useEffect(() => {
+    const unsubscribe = subscribeToMore({
+      document: FAVORITES_SUBSCRIBE,
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData) return prev;
+        const { count, sessionId } = subscriptionData.data.favorites;
+
+        const funcIterator = (objSession: SessionType): SessionType =>
+          objSession.id === sessionId
+            ? { ...objSession, favoriteCount: count }
+            : objSession;
+
+        return {
+          ...prev,
+          advanced: prev.advanced.map(funcIterator),
+          intermediate: prev.intermediate.map(funcIterator),
+          intro: prev.intro.map(funcIterator),
+        };
+      },
+    });
+
+    /**
+     * @description cleanup on unmount
+     * @returns {undefined}
+     */
+    return () => {
+      unsubscribe();
+    };
+  }, [subscribeToMore]);
 
   if (loading) return <p>Loading Sessions...</p>;
   if (error) return <p>Error loading sessions!</p>;
